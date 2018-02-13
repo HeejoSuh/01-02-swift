f87fb5be4433bba1ff30fba499112744574bdc93
HeejoSuh <15654639+HeejoSuh@users.noreply.github.com>
2018-02-13 12:15:26 -0500

    Add files via upload

---
+++ LogProgram.swift
@@ -0,0 +1,57 @@
+//
+
+//  LogProgram.swift
+
+//  Calculates the maximum number of logs that can go on
+
+//  a truck, if you know their length
+
+//
+
+//  Created by Heejo Suh in Feb 2018
+
+//  Copyright @ 2018 MTHS. All rights reserved.
+
+//
+
+
+
+//define variables
+
+var truckCanCarry: Double = 1100
+
+var logWeigh: Double = 20
+
+var numberOfLogsCanCarry : Double
+
+var lengthOfLog : Double = 0
+
+
+
+//ask for lengthof logs
+
+print("Input the length of your logs (0.25 m, 0.5 m or 1m): ")
+
+//get input***********
+
+let input = readLine(strippingNewline: true)
+
+
+
+//check if input is a number (Can't do try)
+
+if let lengthOfLog = Double(input!) {
+
+    //let lengthOfLog = Double(input!)
+
+    let numberOfLogsCanCarry = truckCanCarry / ( logWeigh * lengthOfLog )
+
+    //need to have the !
+
+    print("Maximum number of logs the truck can carry:  \(numberOfLogsCanCarry)")
+
+} else {
+
+    print("ERROR:\nPut in a numeric value. TRY AGAIN. \n\n")
+
+}
