2cd7335de8f9da914e9ad6e9133a4d3c36f96bb6
HeejoSuh <15654639+HeejoSuh@users.noreply.github.com>
2018-02-13 12:23:18 -0500

    Update LogProgram.swift

--- LogProgram.swift
+++ LogProgram.swift
@@ -27,31 +27,36 @@ var numberOfLogsCanCarry : Double
 var lengthOfLog : Double = 0
 
 
+while (true){
+	//ask for lengthof logs
 
-//ask for lengthof logs
+	print("Input the length of your logs (0.25 m, 0.5 m or 1m): ")
 
-print("Input the length of your logs (0.25 m, 0.5 m or 1m): ")
+	//get input***********
 
-//get input***********
-
-let input = readLine(strippingNewline: true)
+	let input = readLine(strippingNewline: true)
 
 
 
-//check if input is a number (Can't do try)
+	//check if input is a number (Can't do try)
 
-if let lengthOfLog = Double(input!) {
+	if let lengthOfLog = Double(input!) {
 
-    //let lengthOfLog = Double(input!)
+  	  //let lengthOfLog = Double(input!)
 
-    let numberOfLogsCanCarry = truckCanCarry / ( logWeigh * lengthOfLog )
+   	 if lengthOfLog==0.25 || lengthOfLog==0.5 || lengthOfLog==1{
+	let numberOfLogsCanCarry = truckCanCarry / ( logWeigh * lengthOfLog )
 
-    //need to have the !
+   		 //need to have the !
+	
+    		print("Maximum number of logs the truck can carry:  \(numberOfLogsCanCarry)")
 
-    print("Maximum number of logs the truck can carry:  \(numberOfLogsCanCarry)")
+		} else{
+		print("Put in 0.25, 0.5, or 1!")
+		}
+	} else {
 
-} else {
+    	print("ERROR:\nPut in a numeric value. TRY AGAIN. \n\n")
 
-    print("ERROR:\nPut in a numeric value. TRY AGAIN. \n\n")
-
+	}
 }
